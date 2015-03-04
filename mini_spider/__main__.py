be92df60f55f86bbde2425b6683ff43e69f21d62
unknown <linnan01@8DA2B3845FFD502.internal.baidu.com>
2015-03-04 20:21:18 +0800

    temp

--- mini_spider/__main__.py
+++ mini_spider/__main__.py
@@ -1,3 +1,29 @@
-__author__ = 'linnan01'
+__author__ = 'cafedeflore'
+#coding=utf-8
 
+import getopt
+import sys
+
+def version():
+    print "version 1.0.0"
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "vhc:")
+    except getopt.GetoptError as err:
+        print str(err) # will print something like "option -a not recognized"
+        sys.exit(2)
+    for o, a in opts:
+        if o == "-v":
+            version()
+            sys.exit()
+        elif o == "-h":
+            print "帮助信息：没有帮助^_^"
+            sys.exit()
+        elif o == "-c":
+            print a
+        else:
+            assert False, "unhandled option"
+
+main()
 print "hello world!"
--- mini_spider/utils/__init__.py
+++ mini_spider/utils/__init__.py
@@ -1,1 +1,2 @@
+__author__ = 'cafedeflore'
 __all__ = ["utils"]
